777b29a26afa45cd3fcfee48ba07aee85299d399
Luiz Kimel <luizgmk@gmail.com>
2023-07-22 09:29:42 +0900

    IDE pointed problems.

--- MockGPS/app/src/main/java/com/navigine/mockgps/MainActivity.java
+++ MockGPS/app/src/main/java/com/navigine/mockgps/MainActivity.java
@@ -2,25 +2,19 @@ package com.navigine.mockgps;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-public class MainActivity extends AppCompatActivity
-{
-    private Fragment mSelectOnMap    = new SelectOnMapFragment();
-    private FragmentManager mManager = getSupportFragmentManager();
+public class MainActivity extends AppCompatActivity {
+    final private Fragment mSelectOnMap = new SelectOnMapFragment();
+    final private FragmentManager mManager = getSupportFragmentManager();
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
